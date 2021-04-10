537f578358e60242d59e508c8de0d77514986fbc
tropicalmentat <gabtorres1011@gmail.com>
2021-04-10 11:44:05 +0000

    Created basic script to retrieve bike parking and return as json

---
+++ index.py
@@ -0,0 +1,39 @@
+# https://dev.to/andrewbaisden/creating-react-flask-apps-that-connect-to-postgresql-and-harperdb-1op0
+import flask
+from sqlalchemy import create_engine
+from flask_cors import CORS
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+
+DATABASE = os.getenv('DATABASE')
+HOSTNAME = os.getenv('HOSTNAME')
+PORT = os.getenv('PORT')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+CORS(app)
+
+try:
+    con = psycopg2.connect(
+    	host=HOSTNAME,
+    	port=PORT,
+        database=DATABASE,
+        user=DATABASE_USERNAME,
+        password=DATABASE_PASSWORD)
+
+    cur = con.cursor()
+
+    @app.route('/')
+    def fetch_all_bike_parking():
+        cur.execute("SELECT * FROM traffic where fclass='parking_bicycle'")
+        rows = cur.fetchall()
+
+        return flask.jsonify(rows)
+
+except Exception as e:
+	print(e)
